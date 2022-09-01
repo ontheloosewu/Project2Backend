fdb3a4d9d56c2f664e51c00eefd01c79e34057c2
jalec <jaleckc@gmail.com>
2022-09-01 10:09:32 -0700

    Added create grade testing

--- src/test/java/dev/thebrogrammers/daycare/GradeRepoTests.java
+++ src/test/java/dev/thebrogrammers/daycare/GradeRepoTests.java
@@ -2,7 +2,9 @@ package dev.thebrogrammers.daycare;
 
 import dev.thebrogrammers.entities.Behavior;
 import dev.thebrogrammers.entities.Grade;
+import dev.thebrogrammers.entities.Student;
 import dev.thebrogrammers.repos.GradeRepo;
+import dev.thebrogrammers.repos.StudentRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +17,14 @@ public class GradeRepoTests {
 
     @Autowired
     GradeRepo gradeRepo;
+    @Autowired
+    StudentRepo studentRepo;
 
     @Test
     public void create_grade(){
-        Grade grade = new Grade(0,1,450000000,"Student has eaten all his vegetables", Behavior.EXCELLENT);
+        Student student = new Student(0,"First","Last","test_guardian");
+        Student savedStudent = this.studentRepo.save(student);
+        Grade grade = new Grade(0, savedStudent.getS_id(), 450000000,"Student has eaten all his vegetables", Behavior.EXCELLENT);
         Grade savedGrade = this.gradeRepo.save(grade);
         System.out.println(savedGrade);
         Assertions.assertNotEquals(0,savedGrade.getG_id());
