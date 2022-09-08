a79db03d8ee3deba647101e6c3edec969976f530
AlexMR-Unity <chaosborne123@gmail.com>
2022-09-08 13:56:05 -0500

    - added a maven dependency for JSONObject - swapped out string returns in controllers for JSONObject.quote(string) to ensure front end receives everything in properly formatted JSON.

--- src/main/java/dev/thebrogrammers/controllers/GradeController.java
+++ src/main/java/dev/thebrogrammers/controllers/GradeController.java
@@ -7,6 +7,8 @@ import dev.thebrogrammers.exceptions.InsufficientPermissionException;
 import dev.thebrogrammers.exceptions.UnauthenticatedException;
 import dev.thebrogrammers.services.GradeService;
 import dev.thebrogrammers.services.JwtService;
+import org.apache.coyote.Response;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -58,9 +60,9 @@ public class GradeController {
         if (role.equals("teacher")) {
             boolean isRemoved = this.gradeService.deleteGradeById(Integer.parseInt(id));
             if (isRemoved) {
-                return "Grade has been deleted.";
+                return JSONObject.quote("Grade has been deleted.");
             } else {
-                return "Grade was not found.";
+                return  JSONObject.quote("Grade was not found.");
             }
         } else {
             throw new InsufficientPermissionException();
